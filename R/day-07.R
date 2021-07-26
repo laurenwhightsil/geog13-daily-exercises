4ebb554b1f05fa2d7b37b6accb71d81e9bad3339
laurenwhightsil <laurenwhightsil@ucsb.edu>
2021-07-26 12:28:43 -0700

    day 7

--- R/day-07.R
+++ R/day-07.R
@@ -3,30 +3,25 @@
 # Purpose: Daily Exercise 7: Faceted Plot of US COVID-19 Cases
 
 library(tidyverse, magrittr)
-url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 covid = read_csv(url)
 
 ## Question 1:
-covid %>%
-  region = data.frame(state = state.name, region = state.region) %>%
-  filter(state %in% state.name) %>%
-  filter(date == max(date)) %>%
-  count(state) %>%
-  left_join(covid, region, by = "state") %>%
-    count(region) %>%
-    mutate(tot = sum(n)) %>%
-  group_by(region, date) %>%
-  summarize(cases = sum(cases, na.rm = TRUE),deaths = sum(deaths, na.rm = TRUE)) %>%
-  pivot_longer(cols = c('cases', 'deaths')) -> covid_region
+region = data.frame(state = state.name, region = state.region)
 
 covid %>%
-  ggplot(covid_region, aes(x = date, y = value)) +
+  right_join(region, by = "state") %>%
+  group_by(region, date) %>%
+  summarize(cases = sum(cases, na.rm = TRUE), deaths = sum(deaths, na.rm = TRUE)) %>%
+  pivot_longer(cols = c('cases', 'deaths')) -> covid_region
+
+ggplot(covid_region, aes(x = date, y = value)) +
     geom_line(aes(col = region)) +
     facet_grid(name~region, scale = "free_y") +
-    theme_linedraw() +
+    theme_grey() +
     theme(legend.position = "NA") +
     labs(title = "Cumulative Cases and Deaths: Region",
        subtitle = "Data Source: NY-Times COVID-19 Data",
        x = "Date", y = "Daily Cumulative Count",
        caption = "Daily Exercise 07")
-  ggsave("exercise07image.png")
+  ggsave("./img/Day-07.png")
